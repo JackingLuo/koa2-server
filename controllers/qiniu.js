148eb6da12aacf38579b6db64f9ab26cf4e9a190
Arbort <781642016@qq.com>
2019-12-19 17:03:47 +0800

    七牛云上传并添加到数据库接口完成

--- controllers/qiniu.js
+++ controllers/qiniu.js
@@ -1,5 +1,5 @@
 /**
- * 文件上传到七牛云
+ * (多)文件上传到七牛云
  */
 let qiniu = require('qiniu');
 let formidable = require('formidable');
@@ -9,7 +9,7 @@ const myQnConfig ={
     ACCESS_KEY: 'AeYncZyDvSgJdl8EXsPKCvXcARkAhxoIZVsQiqHM',
     SECRET_KEY : 'Vh2Y9Vx9V9xScIXm58tJZuPXo3z2Y9ClBWINLoo5',
     bucket:'luojx',
-    myUrl:"http://luojiaxin.com"
+    myUrl:"http://q2hxeye5d.bkt.clouddn.com"
 }
 
 //设置ACCESS_KEY和SECRET_KEY
@@ -17,67 +17,60 @@ qiniu.conf.ACCESS_KEY = myQnConfig.ACCESS_KEY;
 qiniu.conf.SECRET_KEY =myQnConfig.SECRET_KEY;
 
 
-let qn = {};
-
-
-//构建上传策略函数   （获取七牛上传token）
-qn.uptoken = function() {
+//构建上传策略函数  （获取七牛上传token）
+let uptoken = function() {
     //要上传的空间
     var putPolicy = new qiniu.rs.PutPolicy({ scope: myQnConfig.bucket });
     var uploadToken=putPolicy.uploadToken();
     return uploadToken;
 }
 
-qn.uploadImg = function(req,callback){
-    let callbackObj = {
+const qnUpload = (files,callback)=>{
+    let backInfo = {
         succ:false,
-        status:0,
+        data:[],
+        code:0,
         errMsg:''
     };
-    var form = new formidable.IncomingForm();   //创建上传表单
-    form.encoding = 'utf-8';        //设置编辑
-    //form.uploadDir = '../../uploadimg';     //设置上传目录 设置则会存储在中控服务器，不设置则建立一个临时文件 最后要上传到七牛，所以不用设置
-    form.keepExtensions = true;     //保留后缀
-    form.maxFieldsSize =10* 1024 * 1024;   //文件大小10M    报错413上传文件太大了
-    form.parse(req, function(err, fields, files) {
-        if (err) {
-            callbackObj.errMsg = err;
-            return callback(callbackObj);
-        }
-        //上传到七牛后保存的文件名
-        let key = new Date().getTime()+Math.ceil(Math.random()*10);
-        //生成上传 Token
-        let token = qn.uptoken();
-        //要上传文件的本地图片路径
-        let filePath = files.file.path;     //如果是表单的数据提交会存储在fields中，图片文件提交会存储在files中 files.file中的file指的是前端设定input的name一般设置为file
-        
-        //构造上传函数
-        // 文件上传（以下四行代码都是七牛上传文件的配置设置）
-        var config = new qiniu.conf.Config();
-        config.zone = qiniu.zone.zone2;  //设置传输机房的位置根据自己的设置选择(华南是zone2)
-        var formUploader = new qiniu.form_up.FormUploader(config);
-        var putExtra = new qiniu.form_up.PutExtra();
-        formUploader.putFile(token, key, filePath, putExtra, function(respErr,respBody, respInfo) {
-            if (respErr) {
-                callbackObj.errMsg = respErr;
-                return callback(callbackObj);
-            }
-            if (respInfo.statusCode == 200) {//上传成功 
-                // 输出 JSON 格式  xxx填写自己在七牛中设置的自定义域名
-                var response = {
-                    "url":myQnConfig.myUrl+'/'+key
-                };
-                callbackObj.succ = true;
-                callbackObj.status =1;
-                callbackObj.data = response;
-                return callback(callbackObj);
-            } else {//上传失败
-                callbackObj.errMsg = respBody;
-                return callback(callbackObj);
-            }
-        });
-    });
+    //files是form解析出来的files(资源文件)
+    let myFiles = files.file;
+    let count = 0;
+    myFiles.forEach(function(file){
+            var filePath=file.path;//本地路径
+            var fileName = file.name;
+            let extension = fileName.substr(fileName.lastIndexOf("."));//文件名后缀
+            let key = 'tang-'+new Date().getTime()+Math.ceil(Math.random()*100)+extension;//上传到七牛后保存的文件名
+            //生成上传 Token
+            let token = uptoken();
+            // 文件上传（以下四行代码都是七牛上传文件的配置设置）
+            var config = new qiniu.conf.Config();
+            config.zone = qiniu.zone.zone2;  //设置传输机房的位置根据自己的设置选择(华南是zone2)
+            var formUploader = new qiniu.form_up.FormUploader(config);
+            var putExtra = new qiniu.form_up.PutExtra();
+            formUploader.putFile(token, key, filePath, putExtra, function(resErr,resBody, resInfo) {
+                if (resErr) {
+                    backInfo.errMsg = resErr;
+                    return callback(backInfo);
+                }
+                if (resInfo.statusCode == 200) {//上传成功 
+                    // 输出 JSON 格式
+                    var response = {
+                        "url":myQnConfig.myUrl+'/'+key
+                    };
+                    count++;
+                    backInfo.data.push(response);
+                    if(myFiles.length==count){
+                        backInfo.succ = true;
+                        backInfo.code =1;                                                               
+                        return callback(backInfo);
+                    }
+                } else {//上传失败
+                    backInfo.errMsg = resBody;
+                    return callback(backInfo);
+                }
+            })
+    })
 }
 
-module.exports = qn;
+module.exports = qnUpload;
 
--- mysql/upload.js
+++ mysql/upload.js
@@ -1,36 +1,86 @@
 /**
- * 图片(包括富文本编辑器的图片)资源上传
+ * 图片(包括富文本编辑器的图片)资源上传到本地服务器
  */
-// const upload = require("../controllers/koa-multer");
+const upload = require("../controllers/koa-multer");
+const qnUpload = require("../controllers/qiniu");
+let formidable = require('formidable');
+let insert_sql = require("../controllers/mysql.config.js");
 
-// const fn_uploadImg=async(ctx,next)=>{
-//     let err = await upload.single('file')(ctx, next)
-//                 .then(res=>{res})
-//                 .catch(err=>err)
-//     if(err){
-//         ctx.body = {
-//             code:0,
-//             msg : err.message
-//         }
-//     }else{
-//         // CKEditor5上传完照片必须返回json数据
-//         ctx.body = {
-//             'code':'1',
-//             'url': 'http://luojiaxin.com/uploads/'+ctx.req.file.filename//返回文件名
-//         }
-//     }  
-// }
-const qn = require("../controllers/qiniu");
 const fn_uploadImg=async(ctx,next)=>{
-    qn.uploadImg(ctx.req,function(res){
-        console.log('res==>',res);
-        if(res.status == 0){
-            ctx.body =res.data
-        }else{
-            ctx.body =res.data
+    let err = await upload.single('file')(ctx, next)
+                .then(res=>{res})
+                .catch(err=>err)
+    if(err){
+        ctx.body = {
+            code:0,
+            msg : err.message
         }
+    }else{
+        // CKEditor5上传完照片必须返回json数据
+        ctx.body = {
+            'code':'1',
+            'url': 'http://luojiaxin.com/uploads/'+ctx.req.file.filename//返回文件名
+        }
+    }  
+}
+/**
+ * 解析form表单数据
+ */
+let getFormData =(ctx)=>{
+    return new Promise((resolve, reject)=>{
+        let form = new formidable.IncomingForm({//创建上传表单
+            encoding:'utf-8',//编码
+            multiples:true,//支持多图片上传
+            // uploadDir:"public/files",//服务器的上传目录,要上传到七牛，不用设置
+            keepExtensions:true,//保留后缀
+            maxFieldsSize:10* 1024 * 1024 //文件大小限制
+        });
+        form.parse(ctx.req,(err,fields,files)=>{
+            if (err)console.log(err)
+            let userId = fields.userId;
+            let takeTime = fields.takeTime;
+            let upTime = fields.upTime;
+            resolve({userId,takeTime,upTime,files})
+        })
     })
 }
+/**
+ * 上传图片(资源)到七牛云
+ */
+const fn_uploadQn=async(ctx,next)=>{
+    // console.log(ctx.request.body)//空对象,说明node不能直接解析form表单
+    let formData = await getFormData(ctx);
+    //先校验上传人有没有权限
+    let sql_power = `SELECT tang_user.power FROM tang_user WHERE tang_user.id=${formData.userId}`;
+    let [backInfo] =await insert_sql(sql_power);
+    if(backInfo.power!='1'){
+        ctx.response.body = {succ:false,errMsg:"对不起,您没有上传权限"}
+    }else{
+        let myUpload=()=>{
+            return new Promise((resolve, reject)=>{
+                qnUpload(formData.files,(res)=>{
+                    resolve(res)
+                    //在这里直接返回ctx.body会报错404
+                })
+            })
+        }
+        let res = await myUpload();//等待这个异步方法执行完,返回的是照片在七牛云的路径
+        //将返回的信息添加到数据库
+        let sql_add = "INSERT INTO tang_photos(user_id,up_time,take_time,path) VALUE(?,?,?,?)";
+        let count = 0;
+        for(let val of res.data){
+            let values = [formData.userId, formData.upTime, formData.takeTime, val.url];
+            let back = await insert_sql(sql_add, values);
+            if(back){
+                count++;
+                if(count==res.data.length){
+                    ctx.response.body = {succ:true,data:res}
+                }
+            }
+        }                                                           
+    }
+}
 module.exports={
-    "POST/upload/image":fn_uploadImg
+    "POST/upload/image":fn_uploadImg,
+    "POST/api/qiniuUp":fn_uploadQn
 }
